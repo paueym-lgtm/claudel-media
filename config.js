20686505f8e3e38c8938ec6fbcf3a377eeb365c9
paueym-lgtm <paueym@claudel.org>
2026-09-12 23:37:50 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+
+const SUPABASE_URL = "https://ubjzeydhercnyougvsvk.supabase.co";
+
+const SUPABASE_ANON_KEY = "sb_publishable_LyxOmx-CyHwX9Cmm5yqcvA_a1_gEXFn";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
