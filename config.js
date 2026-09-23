8ab9f306813940d33c492385026f44ba1f56617b
paueym-lgtm <paueym@claudel.org>
2026-09-23 15:50:38 -0400

    Supprimer config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-
-const SUPABASE_URL = "https://ubjzeydhercnyougvsvk.supabase.co";
-
-const SUPABASE_ANON_KEY = "sb_publishable_LyxOmx-CyHwX9Cmm5yqcvA_a1_gEXFn";
-
-const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
